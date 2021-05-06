fa510839d6f27d4144d1b3eb1fe7d43da125ac57
pdiazq <pdiazq@hotmail.com>
2021-05-06 11:00:02 -0500

    sticky

--- src/frontend/components/Categories.jsx
+++ src/frontend/components/Categories.jsx
@@ -3,6 +3,8 @@ import '../assets/styles/components/Categories.scss';
 
 const Categories = ({ title, children }) => {
 
+  console.log('categorias', title)
+
   return (
       <div className="Categories">
         <h3 className="categories__title">{title}</h3>
